e21b345840eed38313b5dac4cbc581776b4657e6
Artem <kohanartem@gmail.com>
2023-09-25 13:59:34 +0300

    Исправления после ревью кроме экрана статистики

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Артем Кохан on 17.09.2023.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testViewControllerLight() {
+        let vc = TrackerViewController()
+        
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+    }
+    
+    func testViewControllerDark() throws {
+        let vc = TrackerViewController()
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .dark)))
+    }
+
+}
