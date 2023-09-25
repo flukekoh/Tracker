11721f9862ec1f84fda6a69a67d98d40caf87b52
Artem <kohanartem@gmail.com>
2023-09-25 19:31:02 +0300

    Разбираюсь с ошибкой по ssh сертификатом

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -22,7 +22,7 @@ final class TrackerTests: XCTestCase {
     func testViewControllerLight() {
         let vc = TrackerViewController()
         
-        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light)))
+        assertSnapshot(matching: vc, as: .image(traits: .init(userInterfaceStyle: .light))) // светлая тема
     }
     
     func testViewControllerDark() throws {
